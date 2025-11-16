a1de5c219e2405c21f91f7c41e3897c22896efcc
RzR <108324929+I-RzR-I@users.noreply.github.com>
2025-11-16 19:01:58 +0200

    Add code scanner service.

---
+++ src/CodeSource/Abstractions/ICodeSourceScanner.cs
@@ -0,0 +1,70 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Attributes.CodeSource
+//  Author           : RzR
+//  Created On       : 2025-11-13 18:11
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2025-11-14 19:00
+// ***********************************************************************
+//  <copyright file="ICodeSourceScanner.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using System.Collections.Generic;
+using System.Reflection;
+using CodeSource.Models;
+
+#endregion
+
+namespace CodeSource.Abstractions
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     Interface for code source scanner.
+    /// </summary>
+    /// =================================================================================================
+    public interface ICodeSourceScanner
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Finds the annotations in this collection.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>
+        ///     An enumerator that allows foreach to be used to process the annotations in this
+        ///     collection.
+        /// </returns>
+        /// =================================================================================================
+        public IEnumerable<CodeSourceObjectsResult> FindAnnotations(Assembly assembly);
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Finds the annotations in this collection.
+        /// </summary>
+        /// <param name="assemblyName">Name of the assembly.</param>
+        /// <returns>
+        ///     An enumerator that allows foreach to be used to process the annotations in this
+        ///     collection.
+        /// </returns>
+        /// =================================================================================================
+        public IEnumerable<CodeSourceObjectsResult> FindAnnotations(string assemblyName);
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Finds the annotations in their collections.
+        /// </summary>
+        /// <param name="assemblies">The assemblies.</param>
+        /// <returns>
+        ///     An enumerator that allows foreach to be used to process the annotations in this
+        ///     collection.
+        /// </returns>
+        /// =================================================================================================
+        public IEnumerable<CodeSourceObjectsResult> FindAnnotations(IEnumerable<Assembly> assemblies);
+    }
+}
---
+++ src/CodeSource/Services/CodeSourceScanner.cs
@@ -0,0 +1,61 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Attributes.CodeSource
+//  Author           : RzR
+//  Created On       : 2025-11-13 18:11
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2025-11-14 19:04
+// ***********************************************************************
+//  <copyright file="CodeSourceScanner.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using System.Collections.Generic;
+using System.Reflection;
+using CodeSource.Abstractions;
+using CodeSource.Helpers;
+using CodeSource.Models;
+
+#endregion
+
+namespace CodeSource.Services
+{
+    /// <inheritdoc cref="ICodeSourceScanner" />
+    public class CodeSourceScanner : ICodeSourceScanner
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     The code source annotation scanner instance.
+        /// </summary>
+        /// =================================================================================================
+        public static CodeSourceScanner Instance = new CodeSourceScanner();
+
+        /// <inheritdoc />
+        public IEnumerable<CodeSourceObjectsResult> FindAnnotations(Assembly assembly)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            return CodeSourceHelper.GetCodeSourceAssembly(assembly.FullName);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<CodeSourceObjectsResult> FindAnnotations(string assemblyName)
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            return CodeSourceHelper.GetCodeSourceAssembly(assemblyName);
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+
+        /// <inheritdoc />
+        public IEnumerable<CodeSourceObjectsResult> FindAnnotations(IEnumerable<Assembly> assemblies)
+        {
+            return CodeSourceHelper.GetCodeSourceAssembly(assemblies);
+        }
+    }
+}
