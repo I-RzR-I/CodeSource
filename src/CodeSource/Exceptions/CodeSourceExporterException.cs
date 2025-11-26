e382f2a24e024fc1c906c26e006f0e057f23edf8
RzR <108324929+I-RzR-I@users.noreply.github.com>
2025-11-26 20:31:38 +0200

    [#13] - Add common exception.

---
+++ src/CodeSource/Exceptions/CodeSourceExporterException.cs
@@ -0,0 +1,69 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Attributes.CodeSource
+//  Author           : RzR
+//  Created On       : 2025-11-24 08:11
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2025-11-24 08:17
+// ***********************************************************************
+//  <copyright file="CodeSourceExporterException.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using System;
+
+#endregion
+
+namespace CodeSource.Exceptions
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     Exception for signalling code source exporter errors.
+    /// </summary>
+    /// <seealso cref="T:Exception"/>
+    /// =================================================================================================
+    public class CodeSourceExporterException : Exception
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Gets the exporter format.
+        /// </summary>
+        /// <value>
+        ///     The exporter format.
+        /// </value>
+        /// =================================================================================================
+        public string ExporterFormat { get; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CodeSourceExporterException"/> class.
+        /// </summary>
+        /// <param name="exporterFormat">The exporter format.</param>
+        /// =================================================================================================
+        public CodeSourceExporterException(string exporterFormat) 
+            : base(FormatMessage(exporterFormat))
+        {
+            ExporterFormat = exporterFormat;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Format message.
+        /// </summary>
+        /// <param name="exporterFormat">The exporter format.</param>
+        /// <returns>
+        ///     The formatted message.
+        /// </returns>
+        /// =================================================================================================
+        private static string FormatMessage(string exporterFormat)
+        {
+            return $"Unexpected error occurred while trying to export code history in the format '{exporterFormat}'";
+        }
+    }
+}
---
+++ src/CodeSource/Exceptions/CodeSourceUndefinedExportFormat.cs
@@ -0,0 +1,66 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Attributes.CodeSource
+//  Author           : RzR
+//  Created On       : 2025-11-23 23:11
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2025-11-23 23:15
+// ***********************************************************************
+//  <copyright file="CodeSourceUndefinedExportFormat.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using System;
+
+#endregion
+
+namespace CodeSource.Exceptions
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     A code source undefined export format.
+    /// </summary>
+    /// <seealso cref="T:Exception" />
+    /// =================================================================================================
+    public class CodeSourceUndefinedExportFormat : Exception
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Gets the format to use.
+        /// </summary>
+        /// <value>
+        ///     The format.
+        /// </value>
+        /// =================================================================================================
+        public string Format { get; }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CodeSourceUndefinedExportFormat" /> class.
+        /// </summary>
+        /// <param name="format">Describes the format to use.</param>
+        /// =================================================================================================
+        public CodeSourceUndefinedExportFormat(string format)
+            : base(FormatMessage(format))
+        {
+            Format = format;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Format message.
+        /// </summary>
+        /// <param name="format">Describes the format to use.</param>
+        /// <returns>
+        ///     The formatted message.
+        /// </returns>
+        /// =================================================================================================
+        private static string FormatMessage(string format) => $"Missing exporter for '{format}'";
+    }
+}
