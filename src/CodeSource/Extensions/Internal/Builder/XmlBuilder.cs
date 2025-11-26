f1d35de457d199369c651a57dcd52e1f5799ca6f
RzR <108324929+I-RzR-I@users.noreply.github.com>
2025-11-26 20:36:23 +0200

    [#13] - Add XML export functionality.

---
+++ src/CodeSource/Extensions/Internal/Builder/XmlBuilder.cs
@@ -0,0 +1,202 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Attributes.CodeSource
+//  Author           : RzR
+//  Created On       : 2025-11-19 20:11
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2025-11-20 19:14
+// ***********************************************************************
+//  <copyright file="XmlBuilder.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+#endregion
+
+namespace CodeSource.Extensions.Internal.Builder
+{
+    /// -------------------------------------------------------------------------------------------------
+    /// <summary>
+    ///     An XML builder helper.
+    /// </summary>
+    /// =================================================================================================
+    internal static class XmlBuilder
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     A StreamWriter extension method that writes an XML root.
+        /// </summary>
+        /// <param name="sw">The StreamWriter to act on.</param>
+        /// <param name="value">The value.</param>
+        /// =================================================================================================
+        internal static StreamWriter WriteXmlRoot(this StreamWriter sw, string value)
+        {
+            sw.Write(value);
+            sw.WriteLine();
+
+            return sw;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     A StreamWriter extension method that writes an XML open element.
+        /// </summary>
+        /// <param name="sw">The StreamWriter to act on.</param>
+        /// <param name="name">The element name.</param>
+        /// <param name="indent">The indent.</param>
+        /// <param name="attributes">(Optional) The attributes.</param>
+        /// <param name="inline">(Optional) True to inline element.</param>
+        /// =================================================================================================
+        internal static StreamWriter WriteXmlOpenElement(this StreamWriter sw, string name, string indent,
+            Dictionary<string, string> attributes = null, bool inline = false)
+        {
+            var attributesRow = BuildAttribute(attributes);
+            if (inline)
+            {
+                sw.Write($"<{name}{attributesRow}>");
+            }
+            else
+            {
+                sw.Write(indent);
+                sw.Write('<');
+                sw.Write(name);
+                sw.Write(attributesRow);
+                sw.Write('>');
+            }
+
+            sw.WriteLine();
+
+            return sw;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     A StreamWriter extension method that writes an XML close element.
+        /// </summary>
+        /// <param name="sw">The StreamWriter to act on.</param>
+        /// <param name="name">The element name.</param>
+        /// <param name="indent">The indent.</param>
+        /// =================================================================================================
+        internal static StreamWriter WriteXmlCloseElement(this StreamWriter sw, string name, string indent)
+        {
+            sw.Write(indent);
+
+            sw.Write("</");
+            sw.Write(name);
+            sw.Write('>');
+
+            sw.WriteLine();
+
+            return sw;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     A StreamWriter extension method that writes an XML element.
+        /// </summary>
+        /// <param name="sw">The StreamWriter to act on.</param>
+        /// <param name="indent">The indent.</param>
+        /// <param name="name">The element name.</param>
+        /// <param name="value">The element value.</param>
+        /// <param name="attributes">(Optional) The attributes.</param>
+        /// <param name="inline">(Optional) True to inline element.</param>
+        /// =================================================================================================
+        internal static StreamWriter WriteXmlElement(this StreamWriter sw, string indent, string name, string value,
+            Dictionary<string, string> attributes = null, bool inline = false)
+        {
+            var attributesRow = BuildAttribute(attributes);
+            var escapedValue = value.IfNotMissing(EscapeXml(value));
+
+            if (inline)
+            {
+                sw.Write($"<{name}{attributesRow}>{escapedValue.IfIsNullThenEmpty()}</{name}>");
+            }
+            else
+            {
+                sw.Write(indent);
+
+                sw.Write('<');
+                sw.Write(name);
+                sw.Write(attributesRow);
+                sw.Write('>');
+
+                if (escapedValue.IsPresent())
+                    sw.Write(escapedValue);
+
+                sw.Write("</");
+                sw.Write(name);
+                sw.Write('>');
+            }
+
+            sw.WriteLine();
+
+            return sw;
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Escape XML.
+        /// </summary>
+        /// <param name="source">Source for the XML escape.</param>
+        /// <returns>
+        ///     A string.
+        /// </returns>
+        /// =================================================================================================
+        private static string EscapeXml(string source)
+        {
+            if (source.IsMissing())
+                return source.IfIsNullThenEmpty();
+
+            var sb = new StringBuilder(source.Length + 8);
+            for (var i = 0; i < source.Length; i++)
+            {
+                var c = source[i];
+                switch (c)
+                {
+                    case '&': sb.Append("&amp;"); break;
+                    case '<': sb.Append("&lt;"); break;
+                    case '>': sb.Append("&gt;"); break;
+                    case '\"': sb.Append("&quot;"); break;
+                    case '\'': sb.Append("&apos;"); break;
+                    default: sb.Append(c); break;
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Builds an attribute.
+        /// </summary>
+        /// <param name="attributes">(Optional) The attributes.</param>
+        /// <returns>
+        ///     A string.
+        /// </returns>
+        /// =================================================================================================
+        private static string BuildAttribute(Dictionary<string, string> attributes = null)
+        {
+            if (attributes.HasAnyData())
+            {
+                var attributesRow = string.Empty;
+                if (attributes.HasAnyData())
+                    attributesRow = attributes!.Keys.Aggregate(attributesRow,
+                        (current, key) => current + $" {key} = \"{attributes[key]}\"");
+
+                return attributesRow;
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ src/CodeSource/Services/Export/XmlExporter.cs
@@ -0,0 +1,164 @@
+﻿// ***********************************************************************
+//  Assembly         : RzR.Shared.Attributes.CodeSource
+//  Author           : RzR
+//  Created On       : 2025-11-19 00:11
+// 
+//  Last Modified By : RzR
+//  Last Modified On : 2025-11-19 17:35
+// ***********************************************************************
+//  <copyright file="XmlExporter.cs" company="RzR SOFT & TECH">
+//   Copyright © RzR. All rights reserved.
+//  </copyright>
+// 
+//  <summary>
+//  </summary>
+// ***********************************************************************
+
+#region U S A G E S
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CodeSource.Abstractions;
+using CodeSource.Exceptions;
+using CodeSource.Extensions.Internal;
+using CodeSource.Extensions.Internal.Builder;
+using CodeSource.Models;
+
+// ReSharper disable ConvertToUsingDeclaration
+// ReSharper disable PossibleMultipleEnumeration
+
+#endregion
+
+namespace CodeSource.Services.Export
+{
+    /// <inheritdoc cref="ICodeSourceExporter" />
+    public sealed class XmlExporter : ICodeSourceExporter
+    {
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     (Immutable) the indent. 1 TAB value.
+        /// </summary>
+        /// =================================================================================================
+        private const string Indent = "    ";
+
+        /// <inheritdoc />
+        public string Format { get; } = "XML";
+
+        /// <inheritdoc />
+        public void Export(IEnumerable<CodeSourceObjectsResult> items, Stream outputStream)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(outputStream, new UTF8Encoding(false)))
+                {
+                    sw.WriteXmlRoot("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+                    sw.WriteXmlOpenElement("codeSources", Indent.IndentMultiply(-1));
+
+                    foreach (var item in items) 
+                        WriteItem(sw, item);
+
+                    sw.WriteXmlCloseElement("codeSources", Indent.IndentMultiply(-1));
+                }
+            }
+            catch
+            {
+                throw new CodeSourceExporterException(Format);
+            }
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Writes an item.
+        /// </summary>
+        /// <param name="sw">The StreamWriter.</param>
+        /// <param name="item">The code source object result item.</param>
+        /// =================================================================================================
+        private static void WriteItem(StreamWriter sw, CodeSourceObjectsResult item)
+        {
+            if (item.IsNull()) 
+                return;
+
+            sw.WriteXmlOpenElement("codeSource", Indent.IndentMultiply());
+
+            // Parent
+            sw.WriteXmlOpenElement("parent", Indent.IndentMultiply(1));
+
+            var parent = item.Parent;
+            sw.WriteXmlElement(Indent.IndentMultiply(2), "name", parent.Name);
+            sw.WriteXmlElement(Indent.IndentMultiply(2), "fullName", parent.FullName);
+
+            sw.WriteXmlOpenElement("codeChanges", Indent.IndentMultiply(2));
+            if (parent.History.HasAnyData())
+            {
+                foreach (var h in parent.History)
+                {
+                    sw.WriteXmlOpenElement("codeChange", Indent.IndentMultiply(3));
+
+                    WriteXmlHistoryItem(sw, h, Indent.IndentMultiply(4));
+
+                    sw.WriteXmlCloseElement("codeChange", Indent.IndentMultiply(3));
+                }
+            }
+
+            sw.WriteXmlCloseElement("codeChanges", Indent.IndentMultiply(2));
+            sw.WriteXmlCloseElement("parent", Indent.IndentMultiply(1));
+
+            // Children
+            var children = item.Children;
+            sw.WriteXmlOpenElement("children", Indent.IndentMultiply(1));
+
+            if (children.HasAnyData())
+            {
+                foreach (var child in children)
+                {
+                    sw.WriteXmlOpenElement("child", Indent.IndentMultiply(2));
+                    sw.WriteXmlElement(Indent.IndentMultiply(3), "name", child.Name);
+                    sw.WriteXmlElement(Indent.IndentMultiply(3), "fullName", child.FullName);
+                    sw.WriteXmlOpenElement("codeChanges", Indent.IndentMultiply(3));
+
+                    if (child.History.HasAnyData())
+                    {
+                        foreach (var h in child.History)
+                        {
+                            sw.WriteXmlOpenElement("codeChange", Indent.IndentMultiply(4));
+
+                            WriteXmlHistoryItem(sw, h, Indent.IndentMultiply(5));
+
+                            sw.WriteXmlCloseElement("codeChange", Indent.IndentMultiply(4));
+                        }
+                    }
+
+                    sw.WriteXmlCloseElement("codeChanges", Indent.IndentMultiply(3));
+                    sw.WriteXmlCloseElement("child", Indent.IndentMultiply(2));
+                }
+
+            }
+
+            sw.WriteXmlCloseElement("children", Indent.IndentMultiply(1));
+
+            sw.WriteXmlCloseElement("codeSource", Indent.IndentMultiply());
+        }
+
+        /// -------------------------------------------------------------------------------------------------
+        /// <summary>
+        ///     Writes an XML history item.
+        /// </summary>
+        /// <param name="sw">The StreamWriter.</param>
+        /// <param name="history">The history.</param>
+        /// <param name="indent">(Immutable) the indent. 1 TAB value.</param>
+        /// =================================================================================================
+        private static void WriteXmlHistoryItem(StreamWriter sw, CodeSourceObjectHistory history, string indent)
+        {
+            sw.WriteXmlElement(indent, "codePath", $"{history.CodePath.IfIsNullThenEmpty()}");
+            sw.WriteXmlElement(indent, "sourceUrl", $"{history.SourceUrl.IfIsNullThenEmpty()}");
+            sw.WriteXmlElement(indent, "authorName", $"{history.AuthorName.IfIsNullThenEmpty()}");
+            sw.WriteXmlElement(indent, "copyright", $"{history.Copyright.IfIsNullThenEmpty()}");
+            sw.WriteXmlElement(indent, "appliedOn", $"{history.AppliedOn?.ToString("yyyy-MM-dd")}");
+            sw.WriteXmlElement(indent, "comment", $"{history.Comment.IfIsNullThenEmpty()}");
+            sw.WriteXmlElement(indent, "version", $"{history.Version:##.0##}");
+            sw.WriteXmlElement(indent, "tags", $"{history.Tags.IfIsNullThenEmpty()}");
+            sw.WriteXmlElement(indent, "relatedTaskId", $"{history.RelatedTaskId.IfIsNullThenEmpty()}");
+        }
+    }
+}
